78ae4e1b390eac2a1d7d2fc9ba22b34935e4030e
阳团 <yangtuan@unovo.com.cn>
2019-09-27 18:26:40 +0800

    commint

--- others/MyStore/data/datakitchenMaterial.js
+++ others/MyStore/data/datakitchenMaterial.js
@@ -3,8 +3,24 @@ window.datakitchenMaterial = {
   name: '厨房用料',
   goods: [
     {
+      title: '博泽荣-臻选东北大米-5kg',
+      keywords: ['大米'],
+      pictures: [
+        {
+          type: '星链友店',
+          image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7377353&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: '天猫旗舰店',
+          image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg-tmall.PNG',
+          url: 'https://m.tb.cn/h.eNEoo4U?sm=8bd917',
+        },
+      ]
+    },
+    {
       title: '伊通河-东北吉林小町大米-10kg',
-      keywords: ['关键字'],
+      keywords: ['大米'],
       pictures: [
         {
           type: '星链友店',
@@ -20,7 +36,7 @@ window.datakitchenMaterial = {
     },
     {
       title: '伊通河-东北吉林珍珠米-5kg',
-      keywords: ['关键字'],
+      keywords: ['大米'],
       pictures: [
         {
           type: '星链友店',
@@ -36,7 +52,7 @@ window.datakitchenMaterial = {
     },
     {
       title: '伊通河-东北小粒香米-5kg',
-      keywords: ['关键字'],
+      keywords: ['大米'],
       pictures: [
         {
           type: '星链友店',
@@ -52,7 +68,7 @@ window.datakitchenMaterial = {
     },
     {
       title: '伊通河-田园清香大米-5kg',
-      keywords: ['关键字'],
+      keywords: ['大米'],
       pictures: [
         {
           type: '星链友店',
@@ -68,7 +84,7 @@ window.datakitchenMaterial = {
     },
     {
       title: '伊通河-生态清香大米-5kg',
-      keywords: ['关键字'],
+      keywords: ['大米'],
       pictures: [
         {
           type: '星链友店',
@@ -79,6 +95,70 @@ window.datakitchenMaterial = {
           type: '天猫旗舰店',
           image: './images/datakitchenMaterial/伊通河-生态清香大米-5kg-tmall.PNG',
           url: 'https://m.tb.cn/h.eNvv0PW?sm=cc8c80',
+        },
+      ]
+    },
+    {
+      title: '先农氏-东北小莉香米-5kg',
+      keywords: ['大米'],
+      pictures: [
+        {
+          type: '星链友店',
+          image: './images/datakitchenMaterial/先农氏-东北小莉香米-5kg.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7381632&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: '天猫旗舰店',
+          image: './images/datakitchenMaterial/先农氏-东北小莉香米-5kg-tmall.PNG',
+          url: 'https://m.tb.cn/h.enolP85?sm=4a8a9a',
+        },
+      ]
+    },
+    {
+      title: '先农氏-五常生态香米-5kg',
+      keywords: ['大米'],
+      pictures: [
+        {
+          type: '星链友店',
+          image: './images/datakitchenMaterial/先农氏-五常生态香米-5kg.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7381630&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: '天猫旗舰店',
+          image: './images/datakitchenMaterial/先农氏-五常生态香米-5kg-tmall.PNG',
+          url: 'https://m.tb.cn/h.eNRVfic?sm=ac42d6',
+        },
+      ]
+    },
+    {
+      title: '丑粮-柳河-珍珠香大米-5kg',
+      keywords: ['大米'],
+      pictures: [
+        {
+          type: '星链友店',
+          image: './images/datakitchenMaterial/丑粮-柳河-珍珠香大米-5kg.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7381637&flag=0&storeid=244721&shareuserid=655864&supplierId=2105&starpower=0',
+        },
+        {
+          type: '京东',
+          image: './images/datakitchenMaterial/丑粮-柳河-珍珠香大米-5kg-jd.PNG',
+          url: 'https://item.m.jd.com/product/53605545371.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '金禾世家-五常产区-贡米品质-寒露臻香稻-长粒香-5kg',
+      keywords: ['大米'],
+      pictures: [
+        {
+          type: '星链友店',
+          image: './images/datakitchenMaterial/金禾世家-五常产区-贡米品质-寒露臻香稻-长粒香-5kg.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7381636&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: '京东',
+          image: './images/datakitchenMaterial/金禾世家-五常产区-贡米品质-寒露臻香稻-长粒香-5kg-jd.PNG',
+          url: 'https://item.m.jd.com/product/37018160321.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
         },
       ]
     },
--- others/MyStore/js/main.js
+++ others/MyStore/js/main.js
@@ -1,6 +1,21 @@
+var searchObj = getUrlSearch() || {};
+
+// 置换网页标题
+if (searchObj.title) {
+  document.title = decodeURIComponent(searchObj.title);
+}
+
+// 初始化Vue示例
 var vm = new Vue({
   el: '#goodsBox',
   data: {
+    // 是否处于关键字展示模式
+    isSearch: searchObj.keywords,
+    // 关键字展示用的标题
+    searchTitle: decodeURIComponent(searchObj.title),
+    // 是否显示分组标题
+    showGroupTitle: !searchObj.keywords,
+    // 商品组合
     goodsGroup: [
       window.datakitchenMaterial,  // 厨房用料
       window.datakitchenCookers,  // 厨房用具
@@ -20,3 +35,64 @@ var vm = new Vue({
     ]
   }
 });
+
+
+/**
+ * 获取URL中指定参数的值
+ * 如果找不到目标参数，则返回undefined
+ * 如果缺省参数名称，将返回search组成的对象
+ * @param  {String} strUrl  url地址
+ * @param  {String} strName 参数名称
+ * @return {String|Object|undefined}
+ */
+function getUrlSearch(strUrl, strName) {
+  console.log(strUrl);
+  // 参数智能化选择
+  // 如果不传参数，则表示默认使用当前网页url，并返回search组成的对象
+  if (arguments.length === 0) {
+    strUrl = location.href;
+    strName = undefined;
+  }
+  // 如果传递一个参数，则需要判断传递的是url还是参数名称
+  else if (arguments.length === 1) {
+    // 表示url
+    if (strUrl.indexOf('http') === 0 || strUrl.indexOf('//') === 0) {
+      strName = undefined;
+    } else {
+      strName = strUrl;
+      strUrl = location.href;
+    }
+  }
+
+  // url去除hash
+  strUrl = strUrl.replace(/#.*$/, '');
+
+  var strSplit = strUrl.split('?'),
+    strQuery = strSplit.length > 1 ? strSplit[1] : "",
+    arrQuery = strQuery === '' ? [] : strQuery.split('&'),
+    objQuery = {},
+    arrPart, strValue, i, len, name, value;
+
+  // 将search字符串转换为对象
+  for (i = 0, len = arrQuery.length; i < len; i++) {
+    arrPart = arrQuery[i];
+    name = arrPart.replace(/=.*$/, '');
+    value = arrPart.replace(/^[^=]*=/, '');
+    objQuery[name] = value || '';
+  }
+
+  // 返回对象
+  if (strName === undefined) {
+    return objQuery;
+  }
+  // 返回指定参数
+  else {
+    strValue = objQuery[strName];
+
+    try {
+      return strValue === undefined ? undefined : decodeURIComponent(strValue);
+    } catch (e) {
+      return ""; //使用decodeURIComponent在解码URL中文字符碰到%AF等字符时会抛出URIError: URI malformed异常，虽然可以通过unescape方法解码但最后由于UTF-8编码问题会造成乱码，所以最终索性输出空字符串。
+    }
+  }
+}
