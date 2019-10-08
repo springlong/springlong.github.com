0e8950f9f85c6af823f06f846f7b04f874a34dca
阳团 <yangtuan@unovo.com.cn>
2019-10-08 15:26:57 +0800

    commit

--- others/MyStore/data/dataMaternal.js
+++ others/MyStore/data/dataMaternal.js
@@ -27,5 +27,164 @@ window.dataFood = {
         },
       ]
     },
+    {
+      title: '美素佳儿-美素力-婴儿配方奶粉-1段-罐装-400g',
+      keywords: ['奶粉', '美素佳儿'],
+      goodsPrice: 118,
+      commission: 1.76,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-400g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375800&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 120,
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-400g-tmall.PNG',
+          url: 'https://m.tb.cn/h.en2OvLx?sm=4f4457',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 120,
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-400g-jd.PNG',
+          url: 'https://item.m.jd.com/product/6514032.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '美素佳儿-美素力-婴儿配方奶粉-1段-罐装-900g',
+      keywords: ['奶粉', '美素佳儿'],
+      goodsPrice: 249,
+      commission: 4.85,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-900g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375801&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 268,
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-900g-tmall.PNG',
+          url: 'https://m.tb.cn/h.eNq1w6r?sm=e13371',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 268,
+          image: './images/dataMaternal/美素佳儿-美素力-婴儿配方奶粉-1段-罐装-900g-jd.PNG',
+          url: 'https://item.m.jd.com/product/6727623.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '美素佳儿-金装2段奶粉-2罐-900g-组合装',
+      keywords: ['奶粉', '美素佳儿'],
+      goodsPrice: 480,
+      commission: 4.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/美素佳儿-金装2段奶粉-2罐-900g-组合装.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375802&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 536,
+          image: './images/dataMaternal/美素佳儿-金装2段奶粉-2罐-900g-组合装-tmall.PNG',
+          url: 'https://m.tb.cn/h.enDfR4S?sm=e6ca2d',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 438,
+          image: './images/dataMaternal/美素佳儿-金装2段奶粉-2罐-900g-组合装-jd.PNG',
+          url: 'https://item.m.jd.com/product/7350652.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '美素佳儿-金装3段奶粉-3罐-组合套装',
+      keywords: ['奶粉', '美素佳儿'],
+      goodsPrice: 516,
+      commission: 9.25,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/美素佳儿-金装3段奶粉-3罐-组合套装.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375799&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 456,
+          couponPrice: 408,
+          image: './images/dataMaternal/美素佳儿-金装3段奶粉-3罐-组合套装-tmall.PNG',
+          url: 'https://m.tb.cn/h.enlds45?sm=f017d8',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 716,
+          image: './images/dataMaternal/美素佳儿-金装3段奶粉-3罐-组合套装-jd.PNG',
+          url: 'https://item.m.jd.com/product/100007618986.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '美素佳儿-金装4段奶粉-2罐-组合套装',
+      keywords: ['奶粉', '美素佳儿'],
+      goodsPrice: 338,
+      commission: 4.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/美素佳儿-金装4段奶粉-2罐-组合套装.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375809&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 396,
+          couponPrice: 388,
+          image: './images/dataMaternal/美素佳儿-金装4段奶粉-2罐-组合套装-tmall.PNG',
+          url: 'https://m.tb.cn/h.enl4l11?sm=4640fa',
+        },
+      ]
+    },
+    {
+      title: '皇家美素佳儿-3段幼儿配方奶粉-800g',
+      keywords: ['奶粉', '美素佳儿', '皇家美素佳儿'],
+      goodsPrice: 376,
+      commission: 16.74,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/皇家美素佳儿-3段幼儿配方奶粉-800g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7375810&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 376,
+          image: './images/dataMaternal/皇家美素佳儿-3段幼儿配方奶粉-800g-jd.PNG',
+          url: 'https://item.m.jd.com/product/100004188427.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '皇家美素佳儿-4段儿童配方奶粉-800g',
+      keywords: ['奶粉', '美素佳儿', '皇家美素佳儿'],
+      goodsPrice: 309,
+      commission: 4.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataMaternal/皇家美素佳儿-4段儿童配方奶粉-800g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7368139&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 346,
+          image: './images/dataMaternal/皇家美素佳儿-4段儿童配方奶粉-800g-jd.PNG',
+          url: 'https://item.m.jd.com/product/8232352.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
   ]
 };
