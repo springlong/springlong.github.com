e385ff305f43bc5b37545ba56759cfb94bab5600
阳团 <yangtuan@unovo.com.cn>
2019-10-10 22:38:03 +0800

    commit

--- others/MyStore/data/dataFood.js
+++ others/MyStore/data/dataFood.js
@@ -202,5 +202,273 @@ window.dataFood = {
         },
       ]
     },
+    {
+      title: '海底捞-自煮火锅-组合套餐B-麻辣嫩牛435g-香辣素食400g',
+      keywords: ['海底捞', '自煮火锅', '自助火锅'],
+      goodsPrice: 56,
+      commission: 1.85,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐B-麻辣嫩牛435g-香辣素食400g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226386&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 59.9,
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐B-麻辣嫩牛435g-香辣素食400g-tmall.PNG',
+          url: 'https://m.tb.cn/h.eowFont?sm=9059f8',
+        },
+      ]
+    },
+    {
+      title: '海底捞-自煮火锅-组合套餐C-麻辣嫩牛435g-番茄牛腩365g',
+      keywords: ['海底捞', '自煮火锅', '自助火锅'],
+      goodsPrice: 66,
+      commission: 2.29,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐C-麻辣嫩牛435g-番茄牛腩365g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226446&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 69.9,
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐C-麻辣嫩牛435g-番茄牛腩365g-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoPWQKE?sm=c89ee9',
+        },
+      ]
+    },
+    {
+      title: '海底捞-自煮火锅-组合套餐D-脆爽牛肚435g-番茄牛腩365g',
+      keywords: ['海底捞', '自煮火锅', '自助火锅'],
+      goodsPrice: 66,
+      commission: 2.29,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐D-脆爽牛肚435g-番茄牛腩365g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226448&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 69.9,
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐D-脆爽牛肚435g-番茄牛腩365g-tmall.PNG',
+          url: 'https://m.tb.cn/h.eMKdlUB?sm=d9bfb1',
+        },
+      ]
+    },
+    {
+      title: '海底捞-自煮火锅-组合套餐E-脆爽牛肚435g-麻辣嫩牛435g',
+      keywords: ['海底捞', '自煮火锅', '自助火锅'],
+      goodsPrice: 66,
+      commission: 2.29,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐E-脆爽牛肚435g-麻辣嫩牛435g.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226450&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 69.9,
+          image: './images/dataFood/海底捞-自煮火锅-组合套餐E-脆爽牛肚435g-麻辣嫩牛435g-tmall.PNG',
+          url: 'https://m.tb.cn/h.eowykeP?sm=f98233',
+        },
+      ]
+    },
+    {
+      title: '海底捞-清汤-火锅底料-110g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 39.9,
+      commission: 1.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-清汤-火锅底料-110g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7255882&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-清汤-火锅底料-110g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eowyRKM?sm=1754de ',
+        },
+      ]
+    },
+    {
+      title: '海底捞-清油火锅底料-麻辣味-220g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 39.9,
+      commission: 1.59,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-清油火锅底料-麻辣味-220g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7255930&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-清油火锅底料-麻辣味-220g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eo2g79B?sm=80d295',
+        },
+      ]
+    },
+    {
+      title: '海底捞-番茄火锅底料-酸香味-200g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 38.8,
+      commission: 1.76,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-番茄火锅底料-酸香味-200g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226454&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 43.9,
+          image: './images/dataFood/海底捞-番茄火锅底料-酸香味-200g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoP5vO2?sm=2d1e37',
+        },
+      ]
+    },
+    {
+      title: '海底捞-鲜香菌汤-火锅底料-110g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 39.9,
+      commission: 1.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-鲜香菌汤-火锅底料-110g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226430&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-鲜香菌汤-火锅底料-110g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoP7Ono?sm=0deb92',
+        },
+      ]
+    },
+    {
+      title: '海底捞-上汤三鲜-火锅底料-200g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 39.9,
+      commission: 1.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-上汤三鲜-火锅底料-200g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226438&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-上汤三鲜-火锅底料-200g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoP6Bcq?sm=7d7f61',
+        },
+      ]
+    },
+    {
+      title: '海底捞-醇香牛油-火锅底料-150g-3袋',
+      keywords: ['海底捞', '火锅底料'],
+      goodsPrice: 22,
+      commission: 0.84,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-醇香牛油-火锅底料-150g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226458&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 29.9,
+          image: './images/dataFood/海底捞-醇香牛油-火锅底料-150g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eo2iPeq?sm=7a4594',
+        },
+      ]
+    },
+    {
+      title: '海底捞-麻辣香锅-调味料-220g-3袋',
+      keywords: ['海底捞', '调味料'],
+      goodsPrice: 39.9,
+      commission: 1.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-麻辣香锅-调味料-220g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226428&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-麻辣香锅-调味料-220g-3袋-tmall.PNG',
+          url: 'https://c.tb.cn/h.eo27vW8?sm=052402',
+        },
+      ]
+    },
+    {
+      title: '海底捞-上汤酸菜鱼-调味料-360g-3袋',
+      keywords: ['海底捞', '调味料'],
+      goodsPrice: 39.9,
+      commission: 1.41,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-上汤酸菜鱼-调味料-360g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226443&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 45.9,
+          image: './images/dataFood/海底捞-上汤酸菜鱼-调味料-360g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eMK6tPK?sm=e84dca',
+        },
+      ]
+    },
+    {
+      title: '海底捞-麻辣小龙虾-调味料-200g-3袋',
+      keywords: ['海底捞', '调味料'],
+      goodsPrice: 29.7,
+      commission: 1.19,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-麻辣小龙虾-调味料-200g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7226474&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 15.9*3,
+          couponPrice: 35.78,
+          image: './images/dataFood/海底捞-麻辣小龙虾-调味料-200g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoDXxq7?sm=1027eb',
+        },
+      ]
+    },
+    {
+      title: '海底捞-香辣小龙虾-调味料200g-3袋',
+      keywords: ['海底捞', '调味料'],
+      goodsPrice: 29.7,
+      commission: 1.19,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/dataFood/海底捞-香辣小龙虾-调味料200g-3袋.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7255917&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 15.9*3,
+          couponPrice: 35.78,
+          image: './images/dataFood/海底捞-香辣小龙虾-调味料200g-3袋-tmall.PNG',
+          url: 'https://m.tb.cn/h.eoD3Ma6?sm=b41aae',
+        },
+      ]
+    },
   ]
 };
