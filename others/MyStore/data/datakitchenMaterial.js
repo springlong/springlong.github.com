37db421b0c50359b259c6ce6da5fb529aae541cf
阳团 <yangtuan@unovo.com.cn>
2019-10-24 16:28:47 +0800

    commit

--- others/MyStore/data/datakitchenMaterial.js
+++ others/MyStore/data/datakitchenMaterial.js
@@ -3,24 +3,224 @@ window.datakitchenMaterial = {
   name: '厨房用料',
   goods: [
     {
-      title: '博泽荣-臻选东北大米-5kg',
-      keywords: ['大米', '东北大米', '博泽荣'],
-      goodsPrice: 29.9,
-      commission: 1.14,
+      title: '海天-味极鲜-酱油-1.28L',
+      keywords: ['海天', '酱油', '味极鲜'],
+      goodsPrice: 24.9,
+      commission: 1.10,
       pictures: [
         {
           type: 'star',
-          image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg.PNG',
-          url: 'https://friend.380star.com/goods.do?goodsid=7377353&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.28升.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388433&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 19.9,
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.28升-jd.PNG',
+          url: 'https://item.m.jd.com/product/743696.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
         },
         {
           type: 'tmall',
-          goodsPrice: 39.9,
-          image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg-tmall.PNG',
-          url: 'https://m.tb.cn/h.eNEoo4U?sm=8bd917',
+          goodsPrice: 19.9,
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.28升-tmcs.PNG',
+          url: 'https://m.tb.cn/h.eqL0W77?sm=1d5c3a',
         },
       ]
     },
+    {
+      title: '海天-味极鲜-酱油-1.9L',
+      keywords: ['海天', '酱油', '味极鲜'],
+      goodsPrice: 27.9,
+      commission: 1.28,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.9L.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388476&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 24.9,
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.9L-jd.PNG',
+          url: 'https://item.m.jd.com/product/2680178.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 24.9,
+          image: './images/datakitchenMaterial/海天-味极鲜-酱油-1.9L-tmcs.PNG',
+          url: 'https://c.tb.cn/h.eKvQgBf?sm=7a20bf',
+        },
+      ]
+    },
+    {
+      title: '海天-金标生抽-1.28L-酱油',
+      keywords: ['海天', '酱油', '金标生抽'],
+      goodsPrice: 21.9,
+      commission: 0.97,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-金标生抽-1.28L-酱油.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388434&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 17.2,
+          image: './images/datakitchenMaterial/海天-金标生抽-1.28L-酱油-jd.PNG',
+          url: 'https://item.m.jd.com/product/743688.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 18.9,
+          image: './images/datakitchenMaterial/海天-金标生抽-1.28L-酱油-tmall.PNG',
+          url: 'https://m.tb.cn/h.eq4v3Bo?sm=2fa41c',
+        },
+      ]
+    },
+    {
+      title: '海天-金标生抽-1.9L-酱油',
+      keywords: ['海天', '酱油', '金标生抽'],
+      goodsPrice: 27.9,
+      commission: 1.28,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-金标生抽-1.9L-酱油.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388475&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 22.9,
+          image: './images/datakitchenMaterial/海天-金标生抽-1.9L-酱油-jd.PNG',
+          url: 'https://item.m.jd.com/product/743677.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 24.9,
+          image: './images/datakitchenMaterial/海天-金标生抽-1.9L-酱油-tmcs.PNG',
+          url: 'https://c.tb.cn/h.eqouuua?sm=e9c472',
+        },
+      ]
+    },
+    {
+      title: '海天-特级-金标生抽-500ml-3瓶',
+      keywords: ['海天', '酱油', '金标生抽'],
+      goodsPrice: 32.9,
+      commission: 1.72,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-特级-金标生抽-500ml-3瓶.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388262&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 9.9*3,
+          image: './images/datakitchenMaterial/海天-特级-金标生抽-500ml-3瓶-jd.PNG',
+          url: 'https://item.m.jd.com/product/851968.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 9.9*3,
+          image: './images/datakitchenMaterial/海天-特级-金标生抽-500ml-3瓶-tmcs.PNG',
+          url: 'https://m.tb.cn/h.eKv5roV?sm=c4f916',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 10.9*3,
+          image: './images/datakitchenMaterial/海天-特级-金标生抽-500ml-3瓶-tmall.PNG',
+          url: 'https://m.tb.cn/h.eK7pUTq?sm=11fd44',
+        },
+      ]
+    },
+    {
+      title: '海天-鲜味生抽-酱油-1.28L',
+      keywords: ['海天', '酱油', '鲜味生抽'],
+      goodsPrice: 19.9,
+      commission: 0.88,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-鲜味生抽-酱油-1.28L.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388436&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 16.8,
+          image: './images/datakitchenMaterial/海天-鲜味生抽-酱油-1.28L-jd.PNG',
+          url: 'https://item.m.jd.com/product/743690.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+      ]
+    },
+    {
+      title: '海天-鲜味生抽-酱油-1.9L',
+      keywords: ['海天', '酱油', '鲜味生抽'],
+      goodsPrice: 23.9,
+      commission: 1.06,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-鲜味生抽-酱油-1.9L.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388441&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 18.8,
+          image: './images/datakitchenMaterial/海天-鲜味生抽-酱油-1.9L-jd.PNG',
+          url: 'https://item.m.jd.com/product/743679.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 18.8,
+          image: './images/datakitchenMaterial/海天-鲜味生抽-酱油-1.9L-tmcs.PNG',
+          url: 'https://m.tb.cn/h.eKvSvAV?sm=ccf027',
+        },
+      ]
+    },
+    {
+      title: '海天-上等蚝油-260g-3瓶',
+      keywords: ['海天', '耗油', '上等蚝油'],
+      goodsPrice: 19.9,
+      commission: 0.88,
+      pictures: [
+        {
+          type: 'star',
+          image: './images/datakitchenMaterial/海天-上等蚝油-260g-3瓶.PNG',
+          url: 'https://friend.380star.com/goods.do?goodsid=7388439&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+        },
+        {
+          type: 'jd',
+          goodsPrice: 4.2*3,
+          image: './images/datakitchenMaterial/海天-上等蚝油-260g-3瓶-jd.PNG',
+          url: 'https://item.m.jd.com/product/851963.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+        },
+        {
+          type: 'tmall',
+          goodsPrice: 4.5*3,
+          image: './images/datakitchenMaterial/海天-上等蚝油-260g-3瓶-tmall.PNG',
+          url: 'https://m.tb.cn/h.eqL9M2z?sm=3451dc',
+        },
+      ]
+    },
+    // {
+    //   title: '博泽荣-臻选东北大米-5kg',
+    //   keywords: ['大米', '东北大米', '博泽荣'],
+    //   goodsPrice: 29.9,
+    //   commission: 1.14,
+    //   pictures: [
+    //     {
+    //       type: 'star',
+    //       image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg.PNG',
+    //       url: 'https://friend.380star.com/goods.do?goodsid=7377353&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+    //     },
+    //     {
+    //       type: 'tmall',
+    //       goodsPrice: 39.9,
+    //       image: './images/datakitchenMaterial/博泽荣-臻选东北大米-5kg-tmall.PNG',
+    //       url: 'https://m.tb.cn/h.eNEoo4U?sm=8bd917',
+    //     },
+    //   ]
+    // },
     {
       title: '伊通河-东北吉林小町大米-10kg',
       keywords: ['大米', '伊通河', '小町米'],
