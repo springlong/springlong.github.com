08f10a97fdcc027a02875fe4a9ab135954e08d7f
阳团 <yangtuan@unovo.com.cn>
2019-11-02 18:32:08 +0800

    commit

---
+++ others/MyStore/brand/dataBrand_天下五谷.js
@@ -0,0 +1,205 @@
+window['dataBrand_天下五谷'] = [
+  {
+    title: '天下五谷-长粒香大米-5kg',
+    keywords: ['大米', '天下五谷', '香米'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 89,
+        commission: 6.87,
+        image: './images/datakitchenMaterial/天下五谷-长粒香大米-5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265135&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+    ]
+  },
+  {
+    title: '天下五谷-稻花香大米-5kg',
+    keywords: ['大米', '天下五谷', '香米', '稻花香'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 119,
+        commission: 8.27,
+        image: './images/datakitchenMaterial/天下五谷-稻花香大米-5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265126&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+      {
+        type: 'jd',
+        goodsPrice: 269,
+        image: './images/datakitchenMaterial/天下五谷-稻花香大米-5kg-jd.PNG',
+        url: 'https://item.m.jd.com/product/31328978273.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-原香初榨-花生油-1.8L',
+    keywords: ['食用油', '天下五谷', '花生油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 49.9,
+        commission: 1.10,
+        image: './images/datakitchenMaterial/天下五谷-原香初榨-花生油-1.8L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265108&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 89,
+        image: './images/datakitchenMaterial/天下五谷-原香初榨-花生油-1.8L-jd.PNG',
+        url: 'https://item.m.jd.com/product/37343314972.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-原香初榨-花生油-5L',
+    keywords: ['食用油', '天下五谷', '花生油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 145.9,
+        commission: 7.14,
+        image: './images/datakitchenMaterial/天下五谷-原香初榨-花生油-5L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265101&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 199,
+        image: './images/datakitchenMaterial/天下五谷-原香初榨-花生油-5L-jd.PNG',
+        url: 'https://item.m.jd.com/product/10280967454.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-非转基因-粗粮2代-食用植物调和油-1.8L',
+    keywords: ['食用油', '天下五谷', '非转基因', '食用植物调和油', '植物调和油', '调和油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 39,
+        commission: 1.76,
+        image: './images/datakitchenMaterial/天下五谷-非转基因-粗粮2代-食用植物调和油-1.8L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265091&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 69,
+        image: './images/datakitchenMaterial/天下五谷-非转基因-粗粮2代-食用植物调和油-1.8L-jd.PNG',
+        url: 'https://item.m.jd.com/product/37338842484.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-非转基因-粗粮2代-食用植物调和油-5L',
+    keywords: ['食用油', '天下五谷', '非转基因', '食用植物调和油', '植物调和油', '调和油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 83.9,
+        commission: 3.6,
+        image: './images/datakitchenMaterial/天下五谷-非转基因-粗粮2代-食用植物调和油-5L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265088&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 199,
+        image: './images/datakitchenMaterial/天下五谷-非转基因-粗粮2代-食用植物调和油-5L-jd.PNG',
+        url: 'https://item.m.jd.com/product/1807562273.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-压榨-葵花籽油-5L',
+    keywords: ['食用油', '天下五谷', '葵花籽油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 79,
+        commission: 0.83,
+        image: './images/datakitchenMaterial/天下五谷-压榨-葵花籽油-5L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7281131&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 199,
+        image: './images/datakitchenMaterial/天下五谷-压榨-葵花籽油-5L-jd.PNG',
+        url: 'https://item.m.jd.com/product/11156487203.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-压榨-玉米油-1L',
+    keywords: ['食用油', '天下五谷', '玉米油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 25.9,
+        commission: 1.58,
+        image: './images/datakitchenMaterial/天下五谷-压榨-玉米油-1L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265122&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 49.9,
+        image: './images/datakitchenMaterial/天下五谷-压榨-玉米油-1L-jd.PNG',
+        url: 'https://item.m.jd.com/product/39392668757.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-植物甾醇-玉米油-1.8L',
+    keywords: ['食用油', '天下五谷', '玉米油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 39,
+        commission: 1.76,
+        image: './images/datakitchenMaterial/天下五谷-植物甾醇-玉米油-1.8L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265118&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 49,
+        image: './images/datakitchenMaterial/天下五谷-植物甾醇-玉米油-1.8L-jd.PNG',
+        url: 'https://item.m.jd.com/product/37342006204.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-植物甾醇-玉米油-5L',
+    keywords: ['食用油', '天下五谷', '玉米油'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 79,
+        commission: 2.59,
+        image: './images/datakitchenMaterial/天下五谷-植物甾醇-玉米油-5L.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265111&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 199,
+        image: './images/datakitchenMaterial/天下五谷-植物甾醇-玉米油-5L-jd.PNG',
+        url: 'https://item.m.jd.com/product/38364361478.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+  {
+    title: '天下五谷-五福有礼-礼盒-750ml-5件装',
+    keywords: ['食用油', '天下五谷', '礼盒'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 399,
+        commission: 88.50,
+        image: './images/datakitchenMaterial/天下五谷-五福有礼-礼盒-750ml-5件装.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7265124&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864'
+      },
+      {
+        type: 'jd',
+        goodsPrice: 499,
+        image: './images/datakitchenMaterial/天下五谷-五福有礼-礼盒-750ml-5件装-jd.PNG',
+        url: 'https://item.m.jd.com/product/32901460071.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share'
+      },
+    ]
+  },
+];
