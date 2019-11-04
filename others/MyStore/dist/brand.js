467ec6571aa1f358f740434b5ac31137f81fc7e9
阳团 <yangtuan@unovo.com.cn>
2019-11-04 14:35:10 +0800

    commit

--- others/MyStore/dist/brand.js
+++ others/MyStore/dist/brand.js
@@ -2940,6 +2940,109 @@ window['dataBrand_金禾世家'] = [
       },
     ]
   },
+  {
+    title: '金禾世家-响水产区贡米品质-火山石板米-长粒-5kg',
+    keywords: ['大米', '金禾世家', '贡米'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 139,
+        commission: 28.2,
+        image: './images/datakitchenMaterial/金禾世家-响水产区贡米品质-火山石板米-长粒-5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=6272857&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+      {
+        type: 'jd',
+        goodsPrice: 128,
+        image: './images/datakitchenMaterial/金禾世家-响水产区贡米品质-火山石板米-长粒-5kg-jd.PNG',
+        url: 'https://item.m.jd.com/product/53570130199.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+      },
+    ]
+  },
+  {
+    title: '金禾世家-五常产区贡米品质长粒-寒地稻花香米-2.5kg',
+    keywords: ['大米', '金禾世家', '贡米', '五常产区', '香米', '稻花香'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 65,
+        commission: 7.49,
+        image: './images/datakitchenMaterial/金禾世家-五常产区贡米品质长粒-寒地稻花香米-2.5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=6272858&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+      {
+        type: 'jd',
+        goodsPrice: 59,
+        image: './images/datakitchenMaterial/金禾世家-五常产区贡米品质长粒-寒地稻花香米-2.5kg-jd.PNG',
+        url: 'https://item.m.jd.com/product/37018555603.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+      },
+    ]
+  },
+  {
+    title: '金禾世家-五常寒地-稻花香米-2.5kg-4包',
+    keywords: ['大米', '金禾世家', '贡米', '五常产区', '香米', '稻花香'],
+    isBoutique: true,
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 199,
+        commission: 27.27,
+        image: './images/datakitchenMaterial/金禾世家-五常寒地-稻花香米-2.5kg-4包.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7377443&flag=0&shopbag=1&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+    ]
+  },
+  {
+    title: '金禾世家-五常产区-贡米品质-长粒-寒地稻花香米-5kg',
+    keywords: ['大米', '金禾世家', '贡米', '五常产区', '香米', '稻花香'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 118,
+        commission: 18.94,
+        image: './images/datakitchenMaterial/金禾世家-五常产区-贡米品质-长粒-寒地稻花香米-5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=6272853&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+    ]
+  },
+  {
+    title: '金禾世家-寒地-稻花香米-5kg',
+    keywords: ['大米', '金禾世家', '香米', '稻花香'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 108,
+        commission: 16.48,
+        image: './images/datakitchenMaterial/金禾世家-寒地-稻花香米-5kg.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=6991769&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+      {
+        type: 'jd',
+        goodsPrice: 108,
+        image: './images/datakitchenMaterial/金禾世家-寒地-稻花香米-5kg-jd.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=6991769&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+    ]
+  },
+  {
+    title: '金禾世家-五常寒地-稻花香米-5kg-2件',
+    keywords: ['大米', '金禾世家', '贡米', '五常产区', '香米', '稻花香'],
+    pictures: [
+      {
+        type: 'star',
+        goodsPrice: 199,
+        commission: 23.79,
+        image: './images/datakitchenMaterial/金禾世家-五常寒地-稻花香米-5kg-2件.PNG',
+        url: 'https://friend.380star.com/goods.do?goodsid=7378303&flag=0&storeid=244721&shareuserid=655864&addspuserid=655864',
+      },
+      {
+        type: 'jd',
+        goodsPrice: 108*2,
+        image: './images/datakitchenMaterial/金禾世家-五常寒地-稻花香米-5kg-2件-jd.PNG',
+        url: 'https://item.m.jd.com/product/37018160321.html?wxa_abtest=o&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share',
+      },
+    ]
+  },
 ];
 window['dataBrand_金龙鱼'] = [
   {
