1c86d3807e2292e6bd572943bfc14040a75e0646
springlong <yangtuan2009@126.com>
2020-03-11 23:28:10 +0800

    commit

--- others/MyStore/brand/dataBrand_name.js
+++
@@ -1,3 +0,0 @@
-window['dataBrand_name'] = [
-
-];
--- others/MyStore/data/dataMaternal.js
+++
@@ -1,8 +0,0 @@
-
-window.dataMaternal = {
-  name: '母婴用品',
-  goods: [
-    ...(window['dataBrand_美素佳儿'] || []),
-    ...(window['dataBrand_伊利_金领冠'] || []),
-  ]
-};
